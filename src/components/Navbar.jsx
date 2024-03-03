9a66dc4dfe70d1d2bb504afb44766fb102b44443
username <barasamica@gmail.com>
2024-03-03 13:56:49 +0300

    Added a App.css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,5 +8,17 @@ const Navbar = () => {
     };
 
 
-        
+return (
+    <nav className='navbar'>
+        <div className='logo' onClick={scrollToTop}>
+            <span> Micah Barasa</span>
+        </div>
+        <div className='nav-links'>
+            <Link to='home' smooth={true} duration={500}>Home</Link>
+            <Link to='about' smooth={true} duration={500}>About Me</Link>
+            <Link to='home' smooth={true} duration={500}>Home</Link>
+            <Link to='home' smooth={true} duration={500}>Home</Link>
+        </div>
+    </nav>
+)        
 } 
