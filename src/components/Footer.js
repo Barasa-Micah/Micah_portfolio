ebc05ef97a634dd5c92141ead8c9883bbb858ff6
username <barasamica@gmail.com>
2024-03-04 00:14:14 +0300

    Changed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.footerContent}>
-          <p>&copy; {new Date().getFullYear()} Your Company</p>
+          <p>&copy; {new Date().getFullYear()} micaTech</p>
           <p>Backend Developer: Micah Barasa</p>
           <p>Contact: micadevelops@gmail.com</p>
           <div className={styles.socialMedia}>
-            <a href="https://twitter.com/yourtwitter" target="_blank" rel="noopener noreferrer">
+            <a href="https://x.com/BarasaMicah5?t=Fp-5W5TkmovW_YEFFADg8g&s=09" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-twitter"></i> Twitter
             </a>
-            <a href="https://linkedin.com/in/yourlinkedin" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/mica-barasa/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-linkedin"></i> LinkedIn
             </a>
-            <a href="https://github.com/yourgithub" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Barasa-Micah" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-github"></i> GitHub
             </a>
           </div>
