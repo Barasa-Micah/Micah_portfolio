c9e5d55b1112ce7b965dcbfe5711662c9628b367
username <barasamica@gmail.com>
2024-03-03 13:48:00 +0300

    Created components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link, animateScroll as scroll} from 'react-scroll';
+
+
+const Navbar = () => {
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
+
+        
+} 
